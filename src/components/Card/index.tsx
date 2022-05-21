8ce39617232f294fe783aa0b07a8933fd0c053f7
Artiom Kalugin <a.o.kalugin@gmail.com>
2022-05-21 21:29:47 +0100

    Added layout for card component.

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,10 @@
+import { StyledCard, StyledIcon, StyledH3, StyledP, StyledA } from './style';
+
+export const Card = () => (
+  <StyledCard>
+    <StyledIcon />
+    <StyledH3></StyledH3>
+    <StyledP></StyledP>
+    <StyledA></StyledA>
+  </StyledCard>
+);
---
+++ src/components/Card/style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledCard = styled.div``;
+
+export const StyledIcon = styled.img``;
+
+export const StyledH3 = styled.h3``;
+
+export const StyledP = styled.p``;
+
+export const StyledA = styled.a``;
